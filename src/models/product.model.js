92777a8ae8c2138b3474c7af1704d9d201a98943
Praveen Saw <praveen.kumar.paytm2019@gmail.com>
2023-09-13 14:46:50 +0530

    Create a static model

---
+++ src/models/product.model.js
@@ -0,0 +1,39 @@
+
+export default class ProductModel{
+
+    constructor(_id, _name, _desc, _price, _imageUrl){
+        this._id = _id;
+        this._name = _name;
+        this._desc = _desc;
+        this._price = _price;
+        this._imageUrl = _imageUrl;
+    }
+
+    static get(){
+        return products;
+    }
+}
+
+var products = [
+    new ProductModel(
+      1,
+      'Product 1',
+      'Description for Product 1',
+      19.99,
+      'https://m.media-amazon.com/images/I/51-nXsSRfZL._SX328_BO1,204,203,200_.jpg',
+    ),
+    new ProductModel(
+      2,
+      'Product 2',
+      'Description for Product 2',
+      29.99,
+      'https://m.media-amazon.com/images/I/51xwGSNX-EL._SX356_BO1,204,203,200_.jpg',
+    ),
+    new ProductModel(
+      3,
+      'Product 3',
+      'Description for Product 3',
+      39.99,
+      'https://m.media-amazon.com/images/I/31PBdo581fL._SX317_BO1,204,203,200_.jpg',
+    ),
+  ]
